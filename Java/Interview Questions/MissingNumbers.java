ef30d9f3eb8a106bc30adc795ef6f31508f5022a
Ahmad Raza <raza2157@gmail.com>
2018-11-19 00:34:34 -0500

    Updated gitignore, added new problem

---
+++ Java/Interview Questions/MissingNumbers.java
@@ -0,0 +1,12 @@
+/*
+ * Problem: Given an array containing all the numbers from 1 to n except two,
+ * find the two missing numbers. e.g. - missing([4, 2, 3]) = 1, 5
+ */
+
+public class MissingNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Yo the number is " + i);
+        }
+    }
+}
---
+++ Java/Interview Questions/Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Palindrome {
+  public static void main(String[] args) {
+    // try (Scanner scanner = new Scanner(System.in)) { // Prompts user for input
+    // checks for whether or not it is a string
+    // System.out.println("Please enter your input: ");
+    // String input = scanner.nextLine();
+    for (int i = 0; i < args.length; i++) { // Alternative input method
+      if (stringIsPalindrome(args[i])) {
+        System.out.println("Your input is a palindrome");
+      } else {
+        System.out.println("Your input is NOT a palindrome");
+      }
+    }
+  }
+  // }
+
+  public static boolean stringIsPalindrome(String input) { // Use StringBuilder to create a reverse string of input
+    StringBuilder reverse = new StringBuilder(input.toLowerCase()).reverse();
+    return input.toLowerCase().equals(reverse.toString());
+  }
+
+  // public static boolean numIsPalindrome(int input) {
+  // int reverse = 0;
+  // int count = 0;
+  // while (input %)
+  // }
+}
