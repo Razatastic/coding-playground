331864204905757d455a70dc5cff0015d3038346
Ahmad Raza <raza2157@gmail.com>
2018-12-27 00:17:15 -0500

    Added comments, edge case

--- Java/Interview Questions/MaxDiffOddEven.java
+++ Java/Interview Questions/MaxDiffOddEven.java
@@ -1,13 +1,30 @@
 import java.util.*;
 
 /**
- * MaxDiffOddEven Solution - I initialized both the odd and even
+ * MaxDiffOddEven Solution:
+ * 
+ * 1 - Initialize both the smallest odd and largest even elements in the set
+ * first.
+ * 
+ * 2 - If the number is an odd, set it to a temp variable (in this case it's
+ * possibleOddIndex). You do this because you want to later compare whether a
+ * larger even appears later in the set.
+ * 
+ * 3 - Look for any larger even number in the set. If it exists, set the
+ * smallestOddIndex to possibleOddIndex if possibleOddIndex has a value. This
+ * works because you're already past the index for possibleOddIndex at this
+ * point. Hence, it fills the condition smallestOddIndex < possibleOddIndex.
+ * 
+ * 4 - Lastly, you check if any value hasn't been initialized or if the
+ * smallestOddIndex is greater than the largestEvenIndex. If so, return 0.
+ * Otherwise, return the difference between the two numbers.
+ * 
  */
 public class MaxDiffOddEven {
+
     public static int maxDiff(List<Integer> numList) {
         int smallestOddIndex = -1, largestEvenIndex = -1;
-        int possibleOddIndex = -1; // Used for keeping track of a possible smallest odd index (need to check
-                                   // following values before switching to this value)
+        int possibleOddIndex = -1; // Used for keeping track of a possible smallest odd index
 
         if (numList.size() < 2) { // Check input size
             return 0;
@@ -26,18 +43,21 @@ public class MaxDiffOddEven {
             } else {
                 if (largestEvenIndex == -1) { // Assign initial value for even value
                     largestEvenIndex = i;
-                } else {
-                    if (currentNum > numList.get(largestEvenIndex)) { // Assign smallest odd index to the possible index
-                                                                      // being tracked if an even is found later
-                        largestEvenIndex = i;
+                    if (possibleOddIndex != -1) { // Only assign possible index if it has a value
                         smallestOddIndex = possibleOddIndex;
+                    }
+                } else {
+                    if (currentNum > numList.get(largestEvenIndex)) { // When an even is found, assign smallest odd
+                                                                      // index to the possible index being tracked
+                        largestEvenIndex = i;
+                        if (possibleOddIndex != -1) { // Only assign possible index if it has a value
+                            smallestOddIndex = possibleOddIndex;
+                        }
                     }
                 }
             }
         }
-        if (smallestOddIndex > largestEvenIndex || smallestOddIndex == 1 || smallestOddIndex == -1
-                || largestEvenIndex == 0) { // If odd comes after largest even (e.g. - {12, -11}) or if value never
-                                            // occured
+        if (smallestOddIndex > largestEvenIndex) { // If smallest odd comes after largest even
             return 0;
         } else {
             return numList.get(largestEvenIndex) - numList.get(smallestOddIndex);
@@ -52,6 +72,7 @@ public class MaxDiffOddEven {
         testList.add(1);
         testList.add(3);
         testList.add(-4);
+
         System.out.println("The max difference between the smallest odd and the largest even in the list "
                 + Arrays.toString(testList.toArray()) + " is " + maxDiff(testList));
     }
